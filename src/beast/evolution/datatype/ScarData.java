8348cba93d10f50685a4f0e696ddb2d4bd2bc6c2
Sophie <seidels@ethz.ch>
2020-12-16 14:23:50 +0100

    Added ScarData
    - so far I used IntegerData to represent the sequence states
    - Problem: IntegerData has stateCount=4 while the stateCount of my Sequences varies
    - Solution: Introduce ScarData, which has nrOfStates as Input parameter
    
    Fixes #4

--- src/beast/evolution/datatype/ScarData.java
+++ src/beast/evolution/datatype/ScarData.java
@@ -1,4 +1,50 @@
 package beast.evolution.datatype;
 
-public class ScarData {
+import beast.core.Description;
+import beast.core.Input;
+import beast.evolution.datatype.DataType.Base;
+
+@Description("Datatype for integer sequence representing sequence states.")
+public class ScarData extends Base {
+
+    public Input<Integer> nrOfStatesInput = new Input<>("nrOfStates",
+            "The number of states a sequence can be in.");
+
+    /*public ScarData() {
+        stateCount = nrOfStatesInput.get();
+        mapCodeToStateSet = null;
+        codeLength = 1;
+        codeMap = null;
+    }*/
+
+    public void initAndValidate(){
+        stateCount  = nrOfStatesInput.get();
+        mapCodeToStateSet = null;
+        codeLength = 1;
+        codeMap = null;
+    }
+
+    @Override
+    public String getTypeDescription() {
+        return "scarData";
+    }
+
+    @Override
+    public boolean isAmbiguousCode(int code) {
+        return code < 0;
+    }
+
+    @Override
+    public String getCharacter(int code) {
+        if (code < 0) {
+            return "?";
+        }
+        return code + "";
+    }
+
+    @Override
+    public int[] getStatesForCode(int code) {
+
+        return new int[]{code};
+    }
 }
